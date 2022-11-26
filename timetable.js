473137fb911e7d941cda3a1f8ec7935c4e385a82
smori <summer2788@g.skku.edu>
2022-11-26 16:08:25 +0900

    make server

--- timetable.js
+++ timetable.js
@@ -1,18 +1,33 @@
+const express = require('express');
 const { Builder, By } = require('selenium-webdriver');
+
+const app = express();
+const port = 3001;
+app.get('/', async (request, response) => {
+    // Web Scraping Code here
+    try {
+        const data = await timetable();
+        response.status(200).json(data);
+    } catch (error) {
+        response.status(500).json({
+            message: 'Server error occurred',
+        });
+    }
+});
 
 
 
 async function timetable() {
 
     driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('https://www.youtube.com/c/LambdaTest/videos');
-
-    driver.get('https://everytime.kr/login');
+    await driver.get('https://everytime.kr/login');
 
 
 
 };
 
-
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
 
 
