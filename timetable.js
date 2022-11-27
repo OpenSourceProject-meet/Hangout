7768885d4958ce3e9ee6712a5b4ceedeaadba304
smori <summer2788@g.skku.edu>
2022-11-27 22:29:25 +0900

    make server

--- timetable.js
+++ timetable.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { Builder, By } = require('selenium-webdriver');
+const { Builder, Capabilities, By, until } = require('selenium-webdriver');
+
+
 
 const app = express();
 const port = 3001;
@@ -16,11 +18,28 @@ app.get('/', async (request, response) => {
 });
 
 
-
 async function timetable() {
+    let caps = new Capabilities();
+    caps.setPageLoadStrategy("eager"); //capabilities : for speed up
 
-    driver = await new Builder().forBrowser('chrome').build();
+    driver = await new Builder().withCapabilities(caps).forBrowser('chrome').build();
     await driver.get('https://everytime.kr/login');
+    //input id, pwd.
+    await driver.findElement(By.css('[name="userid"]')).sendKeys("summer2788");
+    await driver.findElement(By.css('[name="password"]')).sendKeys("whddms123!");
+    await driver.findElement(By.css('[type="submit"]')).click();
+
+    //my time table 
+    await driver.get("https://everytime.kr/timetable");
+    //check if time table is fully loaded.
+    await driver.wait(until.elementLocated(By.className('wrap')), 30000, 'Timed out after 30 seconds', 5000);
+    //get timetable
+    let elements = await driver.findElements(By.className('wrap'));
+
+    for (let e of elements) {
+        console.log(await e.getText());
+    }
+
 
 
 
