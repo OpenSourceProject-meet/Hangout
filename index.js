981b1dd3d83702ad477b6f60d243dec09d61e53f
smori <summer2788@g.skku.edu>
2022-11-30 05:45:11 +0900

    made login system

--- index.js
+++ index.js
@@ -6,12 +6,31 @@ const PORT = 3000;
 
 // 정적 파일 불러오기
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true })) //middleware 정의. information coming from form acess 가능하게 함
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs") //동적 파일 rendering
 
 // 라우팅 정의
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+// app.get("/user/login", (req, res) => {
+//     console.log('success');
+//     res.sendFile(__dirname + "/public/html/login.html");
+// });
+
+app.post("/user/accept", (req, res) => {
+    const isValid = true;
+    if (isValid) {
+        user.id = req.body.userid;
+        user.password = req.body.password;
+    }
+    console.log(user);
+    res.redirect("/");
+
+
+
 });
 
 
@@ -21,3 +40,4 @@ app.listen(PORT, () => {
     console.log(`Listen : ${PORT}`);
 });
 
+const user = { id: "000", password: "000" };
