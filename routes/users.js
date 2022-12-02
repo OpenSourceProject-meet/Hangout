be12a4feeae7597d7ac919cddc4d076edfb18027
smori <summer2788@g.skku.edu>
2022-12-02 17:49:29 +0900

    made all

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const { Builder, Capabilities, By, until } = require('selenium-webdriver');
 const user = { id: "000", pw: "000" };
 const subjects = [[], [], [], [], []];
+const friendSubjects = []; //3차원 배열 
 const friends = [];
 const parser = require("node-html-parser");
 
 
-// when valid id+pwd input, => crawling stage 
+// when valid id+pswd input, => crawling stage 
 router.post("/accept", async (req, res) => {
 
     // res.redirect("/");
@@ -28,6 +29,87 @@ router.post("/accept", async (req, res) => {
 
 
 });
+
+router.post("/timetable", async (req, res) => {
+    console.log("응답");
+    console.log(req.body);
+    let checkBoxArray = JSON.stringify(req.body);
+    checkBoxArray = JSON.parse(checkBoxArray);
+    checkBoxArray = checkBoxArray.array;
+    console.log(checkBoxArray.length);
+
+
+    //driver define
+    let caps = new Capabilities();
+    caps.setPageLoadStrategy("normal"); //capabilities : for speed up
+
+    driver = await new Builder().withCapabilities(caps).forBrowser('chrome').build();
+    //순회하며 친구 시간표 크롤링 
+    for (let i = 0; i < checkBoxArray.length; i++) {
+        if (checkBoxArray[i]) {
+            console.log("friends site:  ", friends[i][0]);
+            await driver.get('https://everytime.kr' + friends[i][0]);
+
+            //check if time table is fully loaded.
+            await driver.wait(until.elementLocated(By.xpath('//*[@id="container"]/div/div[2]/table/tbody/tr')), 30000, 'Timed out after 30 seconds', 5000);
+            // Get element with tag name 'table'
+            let element = await driver.findElement(By.xpath('//*[@id="container"]/div/div[2]/table/tbody/tr'));
+
+            // // Get all the elements available with class name 'tablebody'
+            // let elements = await element.findElements(By.className("tablebody"));
+            element = await element.getAttribute('outerHTML');
+            let table = parser.parse(element);
+
+            //friend 순회 
+            let friendSub = [[], [], [], [], []];
+            //td를 뽑아내야함 td 7개 (월,화,수,목,금)
+            const td = table.querySelectorAll("tr > td");
+            for (var j = 0; j < td.length - 2; j++) {
+                let subject = td[j].querySelectorAll(".subject")
+
+                subject.forEach((sb) => {
+                    //make object {height: xx , top: xx}
+                    // console.log("style은 다음과 같다: ", sb.attributes.style);
+                    let tempt = {};
+                    let temp = [];
+                    b = sb.attributes.style.split(';')
+                    // console.log('b: ', b)
+                    for (var k = 0; k < 2; k++) {
+                        c = b[k].split(':')
+                        // console.log('c: ', c)
+                        d = c[1].replace('px', '');
+                        // console.log('d: ', d)
+                        temp.push(parseInt(d, 10));
+                    }
+                    // b.forEach(element => {
+                    //     c = element.split(':')
+                    //     console.log('c: ', c)
+                    //     d = c[1].replace('px', '');
+                    //     temp.push(parseInt(d, 10));
+                    // })
+                    tempt.height = temp[0];
+                    tempt.top = temp[1];
+                    friendSub[j].push(tempt);
+
+
+
+                });
+
+            };
+            //friendSub push
+            friendSubjects.push(friendSub);
+
+
+        }
+
+    }
+
+
+    console.log("friends: ", friendSubjects);
+    res.json(friendSubjects)
+    driver.quit();
+
+})
 
 
 async function timetable() {
@@ -105,6 +187,8 @@ async function timetable() {
         friends.push(temfreind);
 
     }
+
+    driver.quit()
 
 
 };
